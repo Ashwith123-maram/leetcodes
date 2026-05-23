df6d1561d18e9479fb130851ebb139891dc2578e
MaramAshwith <ashwithr17@gmail.com>
2026-05-23 15:23:01 +0530

    Time: 15 ms (9.62%) | Memory: 47.1 MB (14.52%) - LeetSync

---
+++ 136-single-number/single-number.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int singleNumber(int[] nums) {
+        HashSet<Integer>hs=new HashSet<>();
+        for(int i=0;i<nums.length;i++){
+            if(hs.contains(nums[i])){
+                hs.remove(nums[i]);
+            }else{
+
+            
+            hs.add(nums[i]);
+            }
+        }
+        return hs.iterator().next();
+        
+    }
+}
